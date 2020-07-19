03966dbf7f481d272e57284e37d26fd670e871db
lunarok <lunarok@gmail.com>
2020-07-19 18:28:11 +0200

    fix

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -232,12 +232,12 @@ $(function () {
 			$('.weather-day3[data-l1key=' + i + ']').value(data.result.day3[i]);
 		}
 
-		$("#icone-status").attr('class', 'wi ' . data.result.status.icon);
-		$("#icone-hour").attr('class', 'wi ' . data.result.hour.icon);
-		$("#icone-day0").attr('class', 'wi ' . data.result.day0.icon);
-		$("#icone-day1").attr('class', 'wi ' . data.result.day1.icon);
-		$("#icone-day2").attr('class', 'wi ' . data.result.day2.icon);
-		$("#icone-day3").attr('class', 'wi ' . data.result.day3.icon);
+		$("#icone-status").attr('class', 'wi ' + data.result.status.icon);
+		$("#icone-hour").attr('class', 'wi ' + data.result.hour.icon);
+		$("#icone-day0").attr('class', 'wi ' + data.result.day0.icon);
+		$("#icone-day1").attr('class', 'wi ' + data.result.day1.icon);
+		$("#icone-day2").attr('class', 'wi ' + data.result.day2.icon);
+		$("#icone-day3").attr('class', 'wi ' + data.result.day3.icon);
 
 		roseTrace('wind-status',data.result.status.windBearing);
 		roseTrace('wind-hour',data.result.hour.windBearing);
